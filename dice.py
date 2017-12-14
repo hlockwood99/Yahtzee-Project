02a8f7ac1b29d8de6ed9c8b1b833bbb759565ac9
H. Lockwood <hanford95@gmail.com>
2017-12-14 12:01:24 -0500

    Working on Array
    Building new way to put together initial array

--- dice.py
+++ dice.py
@@ -1,33 +1,13 @@
 
 #Yahtzee Game Functions
-
+import random
 class Dice():
-	__init__(self):
-		self.One = int(random.randrange(0,7))
-		self.Two = int(random.randrange(0,7))
-		self.Three = int(random.randrange(0,7))
-		self.Four = int(random.randrange(0,7))
-		self.Five = int(random.randrange(0,7))
-	def DiceRoll():
-		diceArray = [self.One, self.Two, self.Three, self.Four, self.Five]
-
-
-		q = str(input('Would you like to roll the dice? (y/n) ')) 
-		if q == 'y':
-	
-			counter+=1
-			DiceRolled = [One,Two,Three,Four,Five]
-
-			print(DiceRolled)
-
-
-		if q == 'n':
-			print('Game Over')
-
-		else:
-			print('Well that was not one of the two letters we asked for so I guess you dont want to play')
-			print('Game Over')
-
+	def __init__(self):
+		self.diceArray = [random.randint(1,6) for _ in range(5)]
+	def DiceRoll(self):
+		print(self.diceArray)
+Dice = Dice()
+print(Dice)
 # ⚀ ⚁ ⚂ ⚃ ⚄ ⚅
 
 # Yahtzee
