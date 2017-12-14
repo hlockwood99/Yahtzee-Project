84a8b54582e336a5460e8a3cd5a9fddca74e4b06
MichaelZ <mzhou8919@gmail.com>
2017-12-14 11:42:26 -0500

    edits

--- partII-scoring.py
+++ partII-scoring.py
@@ -1,15 +1,70 @@
-def singledigits(dice, num):
-	score = 0
-	for i in dice:
-		if i == int(num):
-			score += int(num)
+class Yahtzee:
+
+	def singledigits(dice, num):
+		score = 0
+		for i in dice:
+			if i == int(num):
+				score += int(num)
+			return score
+
+	def threeofakind(dice):
+		score = 0
+		for i in dice:
+			if dice.count(i) >= 3:
+				score = sum(dice)
+				return score
+			else:
+				return score
+
+	def fourofakind(dice):
+		score = 0
+		for i in dice:
+			if dice.count(i) >= 4:
+				score = sum(dice)
+				return score
+			else:
+				return score
+
+	# def fullhouse(dice):
+	# 	score = 0
+	# 	if 
+
+	def smallstraight(dice):
+		score = 0
+		dice = str(sorted(dice))
+		if '1, 2, 3, 4' or '2, 3, 4, 5' or '3, 4, 5, 6' in dice:
+			score = 30
+			return score
+
+	def largestraight(dice):
+		score = 0
+		dice = str(sorted(dice))
+		if '1, 2, 3, 4, 5' or '2, 3, 4, 5, 6' in dice:
+			score = 40
+			return score
+
+	def yahtzee(dice):
+		score = 0
+		for i in dice:
+			if dice.count(i) == 5:
+				score = 50
+				return score
+			else:
+				return score
+
+	def chance(dice):
+		score = sum(dice)
 		return score
 
-def threeofakind(dice):
-	score = 0
-	for i in dice:
-		if if dice.count(i) >= 3:
-			score = sum(dice)
-			return score
-		else:
-			return score
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ runner.py
@@ -0,0 +1,5 @@
+import Dice
+import Yahtzee
+import random
+
+input('Hi there! Would you like to play Yahtzee? y/n ')
