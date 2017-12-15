bffc0637f020b5c34e2f69a233de478ca43a1546
MichaelZ <mzhou8919@gmail.com>
2017-12-15 13:28:49 -0500

    commit

--- runner.py
+++ runner.py
@@ -2,4 +2,12 @@ import Dice
 import Yahtzee
 import random
 
-input('Hi there! Would you like to play Yahtzee? y/n ')
+response = input('Hi there, friend! Would you like to play Yahtzee? [y] or [n]')
+while response != 'y' and response != 'n':
+	print('Try again. Please type [y] or [n]')
+	response = input('Hi there, friend! Would you like to play Yahtzee? [y] or [n]')
+	if response == 'y':
+		Dice()
+	elif response == 'n':
+		print('Come again soon!')
+		break
