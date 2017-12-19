520ee71d54d9579d243352b8b87f05ab923c14b1
MichaelZ <mzhou8919@gmail.com>
2017-12-19 08:36:32 -0500

    !!!

--- runner.py
+++ runner.py
@@ -12,32 +12,50 @@ while response != 'y' and response != 'n':
 		print('Come again soon!')
 		break
 
-try1 = input('Would you like to roll the dice?')
-while response != 'y' and response != 'n':
-	print('Try again. Please type [y] or [n]')
-	answer = input('Would you like to roll a die? ')
-	if response == 'y':
-		roll1 = Dice()
-		roll1.DiceRoll()
-	elif response == 'n':
-		print('Come again soon!')
-		break
+for i in range 13:
+	while i => 0:
+		try1 = input('Would you like to roll the dice? ')
+		while response != 'y' and response != 'n':
+			print('Try again. Please type [y] or [n]')
+			try1 = input('Would you like to roll the dice? ')
+			if response == 'y':
+				roll.DiceRoll()
+			elif response == 'n':
+				print('Calculating score... ')
+				score = Yahtzeetool.singledigits() + Yahtzeetool.threeofakind() + Yahtzeetool.fourofakind()
+				+ Yahtzeetool.fullhouse() + Yahtzeetool.smallstraight() + Yahtzeetool.largestraight()
+				+ Yahtzeetool.yahtzee() + Yahtzeetool.chance()
+				finalscore.append(score)
 
-try2 = input('Would you like to roll the dice?')
-while response != 'y' and response != 'n':
-	print('Try again. Please type [y] or [n]')
-	answer = input('Would you like to roll a die? ')
-	if response == 'y':
-		roll1 = Dice()
-		roll1.DiceRoll()
-	elif response == 'n':
-		print('Come again soon!')
-		break
+		try2 = input('Would you like to reroll the dice? (Try 2) ')
+		while response != 'y' and response != 'n':
+			print('Try again. Please type [y] or [n]')
+			try2 = input('Would you like to reroll the dice? (Try 2) ')
+			if response == 'y':
+				roll.DiceRoll()
+			elif response == 'n':
+				print('Calculating score... ')
+				score = Yahtzeetool.singledigits() + Yahtzeetool.threeofakind() + Yahtzeetool.fourofakind()
+				+ Yahtzeetool.fullhouse() + Yahtzeetool.smallstraight() + Yahtzeetool.largestraight()
+				+ Yahtzeetool.yahtzee() + Yahtzeetool.chance()
+				finalscore.append(score)
+
+		try3 = input('Would you like to reroll the dice? (Try 3) ')
+		while response != 'y' and response != 'n':
+			print('Try again. Please type [y] or [n]')
+			try3 = input('Would you like to reroll the dice? (Try 3) ')
+			if response == 'y':
+				roll.DiceRoll()
+			elif response == 'n':
+				print('Calculating score... ')
+				score = Yahtzeetool.singledigits() + Yahtzeetool.threeofakind() + Yahtzeetool.fourofakind()
+				+ Yahtzeetool.fullhouse() + Yahtzeetool.smallstraight() + Yahtzeetool.largestraight()
+				+ Yahtzeetool.yahtzee() + Yahtzeetool.chance()
+				finalscore.append(score)
+
 
 Yahtzeetool = Yahtzee()
-
-score = Yahtzeetool.singledigits() + Yahtzeetool.threeofakind() + Yahtzeetool.fourofakind()
-+ Yahtzeetool.fullhouse() + Yahtzeetool.smallstraight() + Yahtzeetool.largestraight()
-+ Yahtzeetool.yahtzee() + Yahtzeetool.chance()
+roll = Dice()
+finalscore = []
 
 print('FINAL SCORE: ' + score)
